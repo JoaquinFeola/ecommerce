22bacfc2924ee7336b74c5497b4401df8086e2dc
Joaquin Feola <joacoarielfeola@gmail.com>
2023-04-28 11:30:09 -0300

    haciendo la tarjeta de confirmacion de compra

--- src/ecommerce/pages/FinishPurchasePage.jsx
+++ src/ecommerce/pages/FinishPurchasePage.jsx
@@ -1,9 +1,41 @@
+import '../assets/css/finishPurchase.css';
+
 
 export const FinishPurchasePage = () => {
-    
-    return (
-    <div className="card">
-        <div className="credit-card"></div>
-    </div>
+
+  return (
+    <>
+      <div className="card">
+        <div className="front__card">
+          <div className="mastercard__img">
+            <img src="/assets/mastercard.png" alt="master card imagen" />
+          </div>
+          <div className="visa__img">
+            <img src="/assets/chip.tarjeta.png" alt="chip de la tarjeta" className='card-img' />
+            <img src="/assets/visa.png" alt="visa image" className='card-img' />
+          </div>
+          <div className="card__number">
+            <p>Número tarjeta</p>
+            <p>1232 3423 3232</p>
+          </div>
+          <div className="card_name-container">
+            <div className='first'>
+              <p>Nombre tarjeta</p>
+              <p>Joaquin robloxiano</p>
+            </div>
+            <div className="card__expiration">
+              <p>Expiración</p>
+              <p>12/24</p>
+            </div>
+          </div>
+        </div>
+        <div className="back__card">
+          <div className="magnetic-band"></div>
+          <div className="security-code-more-container">
+            atras
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
